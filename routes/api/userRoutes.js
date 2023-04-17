2742e308be1ac8adedd1839013b44b4b2c94b04e
Hailey Francis <n01402670@unf.edu>
2023-04-17 17:33:31 -0400

    added userController, working on thoughtController

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,2 +1,25 @@
 const router = require('express').Router();
 const User = require('../models')
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController.js');
+
+//TODO: api/users/:userId/friends/:friendId
+//post to add new friend to users friend list
+//delete to remove a friend from a user's friend list
+
+// /api/Users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/Users/:UserId
+router
+  .route('/:UserId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+module.exports = router;
