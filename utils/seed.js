9ecf763c8216c94c9d271b04aa52c908bb86225f
Hailey Francis <n01402670@unf.edu>
2023-04-18 16:32:43 -0400

    added seed data with help from peers, added .gitignore

--- utils/seed.js
+++ utils/seed.js
@@ -1,51 +1,91 @@
-const connection = require('../config/connection');
-const { Course, Student } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const { getArrayOfNames, getMessages, getReaction } = require("./data");
 
-connection.on('error', (err) => err);
+connection.on("error", (err) => err);
 
-connection.once('open', async () => {
-  console.log('connected');
+connection.once("open", async () => {
+  console.log("connected");
 
-  // Drop existing courses
-  await Course.deleteMany({});
+  // Drop existing users
+  await User.deleteMany({});
 
-  // Drop existing students
-  await Student.deleteMany({});
+  // Drop existing thoughts
+  await Thought.deleteMany({});
 
-  // Create empty array to hold the students
-  const students = [];
+  // Create empty array to hold the users and thoughts
+  const users = [];
 
-  // Loop 20 times -- add students to the students array
+  // Loop 20 times -- add users to the users array
+  let arrayOfNames = getArrayOfNames();
   for (let i = 0; i < 20; i++) {
-    // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
+    const username =
+      arrayOfNames.pop(Math.floor(Math.random() * arrayOfNames.length)) +
+      arrayOfNames.pop(Math.floor(Math.random() * arrayOfNames.length));
+    const email = `${username}@gmail.com`;
+    const thoughts = [];
+    const friends = [];
 
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
+    users.push({
+      username,
+      email,
+      thoughts,
+      friends,
     });
   }
 
-  // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
+  // Add users to the collection and await the results
+  await User.collection.insertMany(users).catch((err)=>{console.log(err)});
 
-  // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...students],
-  });
+  // Add thoughts to thoughts array
+  const messages = getMessages();
+  for (let i = 0; i < 20; i++) {
+    // Get a random user
+    let result = await User.aggregate([{ $sample: { size: 1 } }]);
+    const user = result[0];
+    // console.log(user);
 
-  // Log out the seed data to indicate what should appear in the database
-  console.table(students);
-  console.info('Seeding complete! 🌱');
+    // Create thought fields values
+    const thoughtText = messages[i];
+    const username = user.username;
+
+    // Create reaction fields values
+    const reactionBody = getReaction();
+    result = await User.aggregate([
+      { $match: { _id: { $ne: user._id } } },
+      { $sample: { size: 1 } },
+    ]);
+    const reactionUsername = result[0];
+    const reactions = [
+      { reactionBody: reactionBody, username: reactionUsername.username },
+    ];
+    // console.log(reactions);
+
+    // Insert new Thought
+    const thought = {
+      thoughtText,
+      username,
+      reactions,
+    };
+    const newThought = await Thought.collection
+      .insertOne(thought)
+      .catch((err) => {
+        console.log(err);
+      });
+    // console.log(newThought.insertedId);
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user._id },
+      {
+        $push: {
+          thoughts: [newThought.insertedId],
+          friends: [reactionUsername._id],
+        },
+      },
+      { new: true }
+    );
+    // console.log(updatedUser);
+  }
+
   process.exit(0);
 });
